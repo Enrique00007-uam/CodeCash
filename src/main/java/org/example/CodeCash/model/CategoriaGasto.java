cd74ba80770ea51867a485fc4dc8774bd4d2ccc8
Enrique Miguel Solis Sandoval <164054592+Enrique00007-uam@users.noreply.github.com>
2025-11-22 08:34:54 -0600

    CategoriaGasto modificada

--- src/main/java/org/example/CodeCash/model/CategoriaGasto.java
+++ src/main/java/org/example/CodeCash/model/CategoriaGasto.java
@@ -5,9 +5,11 @@ import lombok.Setter;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
 @Getter
 @Setter
+@Entity
 public class CategoriaGasto extends BaseEntity{
     @Column(length = 50)
     @Required
